76bd4fe0336a2179859418beb391c2d624231eb4
New Island株式会社 <newislandlcc@nyunoMacBook-Pro.local>
2024-05-25 17:02:12 +0900

    Add image and configure HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <img src={require('./assets/my-image.png')} alt="My Image" />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
