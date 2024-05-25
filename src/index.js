e2bcdc61c2cfa1729148aebf072a1bedac25f904
New Island株式会社 <newislandlcc@nyunoMacBook-Pro.local>
2024-05-25 17:44:32 +0900

    Fix image path and confirm build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import myImage from './assets/my-image.png'; // 画像ファイルをインポート
 import './index.css';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <img src={require('./assets/my-image.png')} alt="My Image" />
+      <img src={myImage} alt="My Image" />
     </div>
   );
 }
